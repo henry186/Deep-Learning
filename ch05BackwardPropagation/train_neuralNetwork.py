c592769446e34ec9a5fc3eb85919781eeda192e7
henry186 <pig860930@gmail.com>
2024-05-13 14:14:30 +0800

    updated the comments of ch05 train_neuralNetwork fixed some bugs

--- ch05BackwardPropagation/train_neuralNetwork.py
+++ ch05BackwardPropagation/train_neuralNetwork.py
@@ -13,30 +13,35 @@ iters_num = 70000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+
 train_loss_list = []
 train_acc_list = []
 test_acc_list = []
 
 iter_per_epoch = max(train_size, batch_size, 1)
 
+# training
 for i in range(iters_num):
+    # take a batch of data to train
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
 
-    # backward propagating to calculate gradient
+    # backward propagate to calculate gradient
     grad = network.gradient(x_batch, t_batch)
 
-    # learning the parameters
+    # learn the parameters
     for key in ('W1', 'W2', 'b1', 'b2'):
         network.param[key] -= learning_rate * grad[key]
-    # calculat loss
+
+    # calculate loss
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
     # calculate accuracy every epoch(fully seeing the data)
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_batch, t_batch)
-        test_acc = network.accuracy(x_batch, t_batch)
+        test_acc = network.accuracy(x_test, t_test)
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
         print(train_acc, test_acc)
